a72e55fae52d6d424d24b0db3c782c7b11791dc2
ismail feham <ismail.feham64@gmail.com>
2024-05-09 16:01:03 -0400

    added types

--- src/constants/types.d.ts
+++ src/constants/types.d.ts
@@ -1,5 +1,30 @@
-declare type ApplicationType = {
+import {IconName} from "@/assets/icons";
+import React from "react";
+
+declare type WindowAppProps = {
+    onClose: () => void;
+    onMinimize: () => void;
+    onMaximize: () => void;
+}
+
+declare type ExtendedApplicationProps<T> = T & WindowAppProps;
+
+declare type DesktopWindow = {
+    zIndex: number;
     name: string;
-    icon: IconName
+    minimized: boolean;
+    icon: IconName;
+    component: React.ReactElement;
+}
+
+declare type DesktopWindows = {key
+    [key: string]: DesktopWindow
+}
+
+declare type ApplicationType = {
+    key: string;
+    name: string;
+    icon: IconName,
+    component: React.ReactElement,
 
 }
