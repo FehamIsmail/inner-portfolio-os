ea6a28daaec29e031609e0cf115846554dbadd27
ismail feham <ismail.feham64@gmail.com>
2024-05-09 18:21:46 -0400

    changed font and battery icon

--- src/components/os/Desktop.tsx
+++ src/components/os/Desktop.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import Taskbar from "@/components/os/Taskbar";
+import {Nunito} from 'next/font/google'
 
+const manjari = Nunito({
+    weight: ['400', '700'],
+    subsets: ['latin']}
+);
 
 function Desktop() {
     return (
-        <div></div>
+        <main className={"w-screen h-screen p-0 m-0 bg-[#ffe28a] " + manjari.className}>
+            <div className="relative top-[calc(100%-52px)]">
+                <Taskbar
+                    toggleMinimize={() => {}}
+                    windows={{} as any}
+                />
+            </div>
+        </main>
+
     );
 }
 
